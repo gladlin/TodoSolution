8f3c7712d69580da557a996e1f5db709f5d2effb
gladlin <teslyukalina@yandex.ru>
2025-11-22 17:03:59 +0700

    Практическая 4

--- Todo/TodoItem.cs
+++ Todo/TodoItem.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿// <copyright file="TodoItem.cs" company="Natk">
+// Copyright (c) Natk. All rights reserved.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Todo
+namespace Todo.Core
 {
     public class TodoItem
     {
@@ -12,19 +16,25 @@ namespace Todo
         public string Title { get; set; } = string.Empty;
         public bool IsDone { get; set; }
 
-        public TodoItem() { }
+        public TodoItem()
+        {
+        }
 
         public TodoItem(string title)
         {
             Title = title?.Trim() ?? throw new ArgumentNullException(nameof(title));
         }
+
         public void MarkDone() => IsDone = true;
         public void MarkUndone() => IsDone = false;
         public void Rename(string newTitle)
         {
-            if (string.IsNullOrWhiteSpace(newTitle)) throw new ArgumentException("Title required", nameof(newTitle));
+            if (string.IsNullOrWhiteSpace(newTitle))
+            {
+                throw new ArgumentException("Title required", nameof(newTitle));
+            }
+
             Title = newTitle.Trim();
         }
-
     }
 }
--- Todo/TodoList.cs
+++ Todo/TodoList.cs
@@ -1,51 +1,63 @@
-﻿using System;
+﻿// <copyright file="TodoList.cs" company="Natk">
+// Copyright (c) Natk. All rights reserved.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Todo
+namespace Todo.Core
 {
     public class TodoList
     {
-        public readonly List<TodoItem> _items = new();
-        public IReadOnlyList<TodoItem> Items => _items.AsReadOnly();
+        private readonly List<TodoItem> itemsValue = new();
+        public IReadOnlyList<TodoItem> Items => itemsValue.AsReadOnly();
 
         public TodoItem Add(string title)
         {
             var item = new TodoItem(title);
-            _items.Add(item);
+            itemsValue.Add(item);
             return item;
         }
 
 
-        public bool Remove(Guid id) => _items.RemoveAll(i => i.Id == id) > 0;
+        public bool Remove(Guid id) => itemsValue.RemoveAll(i => i.Id == id) > 0;
 
         public IEnumerable<TodoItem> Find(string substring) =>
-            _items.Where(i => i.Title.Contains(substring ?? string.Empty,
-            StringComparison.OrdinalIgnoreCase));
+            itemsValue.Where(i => i.Title.Contains(
+                substring ?? string.Empty,
+                StringComparison.OrdinalIgnoreCase));
 
-        public int Count => _items.Count;
+        public int Count => itemsValue.Count;
 
+        private static readonly JsonSerializerOptions SwriteOptions = new()
+        {
+            WriteIndented = true,
+        };
         public void Save(string path)
         {
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            string jsonString = JsonSerializer.Serialize(_items, options);
+            string jsonString = JsonSerializer.Serialize(itemsValue, SwriteOptions);
             File.WriteAllText(path, jsonString);
         }
 
         public void Load(string path)
         {
             if (!File.Exists(path))
+            {
                 throw new FileNotFoundException("Файл не найден", path);
+            }
 
             string jsonString = File.ReadAllText(path);
             var loadedItems = JsonSerializer.Deserialize<List<TodoItem>>(jsonString);
 
-            _items.Clear();
+            itemsValue.Clear();
             if (loadedItems != null)
-                _items.AddRange(loadedItems);
+            {
+                itemsValue.AddRange(loadedItems);
+            }
         }
     }
 }
